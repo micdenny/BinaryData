b61c781feaf8ac501d8de6a9e793e0573100e336
Dan McCarthy <wererabbbit@yahoo.com>
2018-04-19 02:00:34 -0500

    Added Binary Stream Seek Unit Test
    -Added Unit Test to confirm Seek method is performing correctly. This
    also creates the BinaryStreamTests.cs class where further general
    BinaryStream Unit Tests can be added.

---
+++ src/Syroot.BinaryData.UnitTest/BinaryStreamTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Syroot.BinaryData.UnitTest
+{
+    [TestClass]
+    public class BinaryStreamTests
+    {
+        // ---- METHODS (PUBLIC) ---------------------------------------------------------------------------------------
+
+        [TestMethod]
+        public void Seek()
+        {
+            using (MemoryStream stream = new MemoryStream())
+            using (BinaryStream binaryStream = new BinaryStream(stream))
+            {
+                Assert.AreEqual(0, binaryStream.Position);
+
+                stream.Seek(2);
+                Assert.AreEqual(2, binaryStream.Position);
+
+                stream.Seek(2);
+                Assert.AreEqual(4, binaryStream.Position);
+            }
+        }
+
+    }
+}
