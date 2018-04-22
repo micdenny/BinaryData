3f4bec11975a86c9425b97fc3b411eafcec7de18
Dan McCarthy <wererabbbit@yahoo.com>
2018-04-22 17:20:54 -0500

    Expanded Binary Stream Seek Unit Test
    -Updated unit test to include changes with different SeekOrigins and
    tests negative seeking.

--- src/Syroot.BinaryData.UnitTest/BinaryStreamTests.cs
+++ src/Syroot.BinaryData.UnitTest/BinaryStreamTests.cs
@@ -15,14 +15,40 @@ namespace Syroot.BinaryData.UnitTest
             using (MemoryStream stream = new MemoryStream())
             using (BinaryStream binaryStream = new BinaryStream(stream))
             {
+                // Prepare stream by writing data.
+                binaryStream.WriteUInt32(0xFFFFFFFF);
+
+                // Check that the stream position can be set from the Beginning of the stream.
+                binaryStream.Seek(0, SeekOrigin.Begin);
                 Assert.AreEqual(0, binaryStream.Position);
 
-                stream.Seek(2);
+                // Check that the stream can seek forwards.
+                binaryStream.Seek(2);
                 Assert.AreEqual(2, binaryStream.Position);
 
-                stream.Seek(2);
-                Assert.AreEqual(4, binaryStream.Position);
+                // Check that the stream can seek backwards.
+                binaryStream.Seek(-2);
+                Assert.AreEqual(0, binaryStream.Position);
+
+                // Check that the stream can seek forwards from the current origin.
+                binaryStream.Seek(2, SeekOrigin.Current);
+                Assert.AreEqual(2, binaryStream.Position);
+
+                // Check that the stream can seek to End of the stream.
+                binaryStream.Seek(0, SeekOrigin.End);
+                Assert.AreEqual(binaryStream.Length, binaryStream.Position);
+
+                // Check that the stream can seek backwards from the end of the stream.
+                binaryStream.Seek(-2, SeekOrigin.End);
+                Assert.AreEqual(binaryStream.Length - 2, binaryStream.Position);
+
+                // Check that the stream can seek forwards from the beginning origin.
+                binaryStream.Seek(2, SeekOrigin.Begin);
+                Assert.AreEqual(2, binaryStream.Position);
+
             }
+
+
         }
 
     }
